6070861239a6d37363f3d537a171a048e380c48a
solohad1n <fox663399@gmail.com>
2022-07-31 15:33:39 +0600

    created and connected the login and registration page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Categories from '../views/Categories.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'main' },
+    component: Categories
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'empty' },
+    component: Login
   },
 ]
 
